4a12a7464706d04e9bb030ed02462097ddc99f34
clencat <37403253+a145789@users.noreply.github.com>
2023-01-04 16:39:48 +0800

    chore: use unbuild

--- bin/tosetup.mjs
+++ bin/tosetup.mjs
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 "use strict";
-import "../dist/setup.js";
+import "../dist/setup.mjs";
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["./src/index.ts", "./src/setup.ts"],
-  format: ["esm"],
-  target: "node14",
-  clean: true,
-  dts: true,
-});
